4327ff6d7dbe8f28ba1bda9efa09e771dca10993
Cezary Bartoszuk <cezarybartoszuk@gmail.com>
2012-03-05 19:57:05 +0100

    Model selection 2: Validation fixed.

--- rLabs/lab03-modelSelection2validation.r
+++ rLabs/lab03-modelSelection2validation.r
@@ -1,7 +1,7 @@
 source("lab03-modelSelection.r")
 
 # Validation
-(function (sampleSize, universeSize, distribution, universe, loopSteps, testSize) {
+(function (sampleSize, universeSize, distribution, universe, loopSteps, testSize, credibility) {
     print("[Validation]")
     learningSampleIndices <- sample(1:universeSize, size=sampleSize, replace=FALSE, prob=distribution)
     learningSample <- universe[learningSampleIndices, ]
@@ -9,19 +9,25 @@ source("lab03-modelSelection.r")
     fullTestSampleIndices <- 1:universeSize
     fullTestSample <- universe[fullTestSampleIndices, ]
     fullTestSampleActualDecisions <- universe$decision[fullTestSampleIndices]
-    classifier <- kknn(decision~., learningSample, universe[1:100, ], k=1)
+    classifier <- kknn(decision~., learningSample, learningSample, k=1)
     fullTestSamplePredictedDecisions <- predict(classifier, fullTestSample, type="class")
     generalizationError <- mean(fullTestSampleActualDecisions != fullTestSamplePredictedDecisions)
     print(sprintf("Generalization error: %f", generalizationError))
-    errorsDiff <- rep(0, loopSteps)
+    empiricErrors <- rep(0, loopSteps)
     for (ii in 1:loopSteps) {
         testSampleIndices <- sample(1:universeSize, size=testSize, replace=FALSE, prob=distribution)
         testSample <- universe[testSampleIndices, ]
         testSampleActualDecisions <- universe$decision[testSampleIndices]
         testSamplePredictedDecisions <- predict(classifier, testSample, type="class")
-        empiricError <- mean(testSampleActualDecisions != testSamplePredictedDecisions)
-        errorsDiff[ii] <- generalizationError - empiricError
-        print(sprintf("[%d] Empiric error: %f", ii, empiricError))
+        empiricErrors[ii] <- mean(testSampleActualDecisions != testSamplePredictedDecisions)
+        print(sprintf("[%d] Empiric error: %f", ii, empiricErrors[ii]))
     }
-    print(sprintf("Mean difference between generalization error and empiric error: %f", mean(errorsDiff)))
-})(1000, UniverseSize, Distribution, Universe, 10000, 1000)
+    print(sprintf("Mean difference between generalization error and empiric error: %f",
+        mean(rep(generalizationError, loopSteps) - empiricErrors)))
+    sortedEmpiricErrors <- sort(empiricErrors)
+    bound <- sortedEmpiricErrors[round(loopSteps * (1.0 - credibility))]
+    epsilon <- generalizationError - bound
+    print(sprintf("E(fU) < ET(fU) + %f with credibility of %f", epsilon, credibility))
+    hoeffdingEpsilon <- sqrt(-log(1 - credibility)/(2 * testSize))
+    print(sprintf("Epsilon given by Hoeffding inequality: %f", hoeffdingEpsilon))
+})(1000, UniverseSize, Distribution, Universe, 10000, 1000, 0.95)
